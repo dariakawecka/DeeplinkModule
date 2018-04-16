f3997c99ac5eb88af6ea0b3a180babeabe344a45
dariakawecka <daria.kawecka@gmail.com>
2018-04-16 09:31:17 -0500

    Url decoding after split of arguments.
    If we url decode before the split of arguments then if an argument value has an '&' in it it will not be parsed correctly. Example : http://localhost:8080/like/entityoverview/name=Johnson+%26+Johnson will not come through correctly.

--- src/DeepLinkModule/javasource/deeplink/actions/ExecuteDeeplink.java
+++ src/DeepLinkModule/javasource/deeplink/actions/ExecuteDeeplink.java
@@ -77,13 +77,12 @@ public class ExecuteDeeplink extends CustomJavaAction<Boolean>
                     Map<String, IDataType> params = Core.getInputParameters(link.getMicroflow());
                     Map<String, Object> args = new HashMap<String, Object>();
                     String allArguments = this.pendinglink.getStringArgument();
-                    
-                    allArguments = URLDecoder.decode(allArguments,"UTF-8");
+                   
                     // If we should separate the GET params, and there is at least one, process them
                     if (link.getSeparateGetParameters() && (allArguments.contains("=") || allArguments.contains("&"))) {
                         String[] arguments = allArguments.split("&");
                         for (String argument : arguments) {
-                            processArgument(argument, params, args);
+                            processArgument(URLDecoder.decode(argument,"UTF-8"), params, args);
                         }
                     // When we don't have a = or & in the arguments, there is no get param
                     // Or if we shouldn't separate, just put the entire string in the first String argument
